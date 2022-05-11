7b4c61f803daee4b84db49c1d55076de105507d3
nate <7259@pm.me>
2022-05-11 11:28:01 +0700

    Include build

--- dist/index.js
+++ dist/index.js
@@ -8,9 +8,10 @@ const core = __nccwpck_require__(186);
 const exec = __nccwpck_require__(514);
 
 async function installAb() {
+   const folder = core.getInput("folder") || "AppBuilder";
+   const stack = core.getInput("stack") || "ab";
    const installOpts = [
-      "",
-      `stack=${core.getInput("stack") || "ab"}`,
+      `stack=${stack}`,
       `port=${core.getInput("port") || 80}`,
       "db.expose=false",
       "db.encryption=false",
@@ -25,21 +26,44 @@ async function installAb() {
       `tenant.url=http://localhost:${core.getInput("port") || 80}`,
    ];
 
-   core.info("Initiliaze Docker Swarm");
+   core.startGroup("Initiliaze Docker Swarm");
    await exec("docker swarm init");
+   core.endGroup();
 
-   core.info("Install AppBuilder");
-   await exec(
-      `npx digi-serve/ab-cli install ${
-         core.getInput("folder") || "AppBuilder"
-      }`,
-      installOpts
-   );
+   core.startGroup("Install AppBuilder");
+   await exec(`npx digi-serve/ab-cli install ${folder}`, installOpts);
+   core.endGroup();
+
+   core.info("Waiting for the Stack to come down");
+
+   await waitClosed();
 
    core.info("Done");
 }
 
 module.exports = installAb;
+
+async function waitClosed(stack) {
+   let output = "";
+
+   const options = {};
+   options.listeners = {
+      stdout: (data) => {
+         output += data.toString();
+      },
+   };
+
+   exec(`docker network ls`, [], options);
+
+   if (output.includes(stack)) {
+      // stack is found so:
+      setTimeout(() => {
+         waitClosed(stack);
+      }, 1000);
+   } else {
+      return;
+   }
+}
 
 
 /***/ }),
